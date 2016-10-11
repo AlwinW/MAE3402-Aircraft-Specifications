b46fbb7c87e393970ca876fa5f9717c758f52119
Alwin <alwang18@hotmail.com>
2016-10-11 21:35:31 +1100

    Constraint Analysis Finalised

--- Helper Constraint Analysis.R
+++ Helper Constraint Analysis.R
@@ -9,13 +9,13 @@
 
 varWS = seq(1500,3500, by = 250)
 varClhls = seq(0.9,1.3, by = 0.1)
-varWbW0_Max = seq(0.6,0.64, by = 0.01)
+varWbW0_Max = seq(0.525,0.65, by = 0.025)
 
 constraint <- inp %>%
   select(-S, -b, -m, -W, -P0eng, -P0) %>%
   mutate(h = AltCruise,
-         Etaprop = 0.77,
-         Etatotal = 0.73,
+         Etaprop = 0.85,
+         Etatotal = 0.80,
          BatteryFactor = 1.0) %>%
   StandardAtomsphere(.)
 
@@ -62,16 +62,11 @@ constraint <- constraint %>%
     WbW0 = (qinf  * Cd0 / WS + K / qinf * WS ) * BatteryFactor * g_sl / Etatotal,
     WbW0Cd0 = (qinf  * Cd0 / WS) * BatteryFactor * g_sl / Etatotal,
     WbW0K = (K / qinf * WS ) * BatteryFactor * g_sl / Etatotal,
-    WS_WbW0_Max = (WbW0_Max * qinf - sqrt(qinf^2 * (WbW0_Max^2 - 4*Cd0*(BatteryFactor*g_sl/Etatotal)^2*K))) / (2*(BatteryFactor*g_sl/Etatotal)*K)
+    WS_WbW0_Max = (WbW0_Max * qinf - sqrt(qinf^2 * (WbW0_Max^2 - 4*Cd0*(BatteryFactor*g_sl/Etatotal)^2*K))) / 
+      (2*(BatteryFactor*g_sl/Etatotal)*K)
   )
 
-# print(constraint)
-
-
-
-
-
-## Plot ======================================================================
+## Constraint Plot ======================================================================
 ggplot(data = constraint, aes(x = WS, group = Clhls)) +
   geom_vline(aes(xintercept = WS_App, colour = "Landing")) +
   geom_line(aes(y = PW_TOFL, colour = "Takeoff")) +
@@ -87,26 +82,38 @@ ggplot(data = constraint, aes(x = WS, group = Clhls)) +
   geom_label(aes(x = WS_App, y = 12, label = sprintf("%0.2f", Clhls), 
                  colour = "Landing"), size = rel(3), show.legend = FALSE) + 
   # WbW0 Labels
-  geom_label(aes(x = WS_WbW0_Max, y = 11, label = sprintf("%0.2f", WbW0_Max), 
+  geom_label(aes(x = WS_WbW0_Max, y = 10, label = sprintf("%0.2f", WbW0_Max), 
                  colour = "Wb/W0"), size = rel(3), show.legend = FALSE) + 
   # Clstar Labels
-  geom_label(aes(x = WS_Clstar, y = 13, label = sprintf("Cl*"), 
+  geom_label(aes(x = WS_Clstar, y = 14, label = sprintf("Cl*"), 
                  colour = "Clstar"), size = rel(3), show.legend = FALSE) + 
   # Cruise Labels
   geom_point(aes(y = PW_Cruise, colour = "Cruise")) +
   geom_label(aes(y = PW_Cruise, label = sprintf("%0.2f", Clcruise), 
                  colour = "Cruise"), size = rel(3), vjust = 1.5, show.legend = FALSE) +
   # Takeoff Labels
-  geom_label(data = filter(constraint, WS == varWS[2]),
+  geom_label(data = filter(constraint, WS == varWS[3]),
              aes(x = WS, y = PW_TOFL, label = sprintf("%0.2f", Clhls), 
                colour = "Takeoff"), size = rel(3), show.legend = FALSE) + 
   # Us
   geom_point(data = inp, aes(x = WS, y = P0/W, label = "Design")) +
-  xlab("Wing Loading") +
-  ylab("")
+  xlab("Wing Loading (N/m^2)") +
+  ylab("Power Loading (W/N)") +
+  ggtitle("Constraint Analysis") +
+  geom_label(x = -Inf, y = Inf, vjust = 1.5, hjust = -0.1,
+             label = "Constant Cd0, AR, e and eta")
 
-# ggplot(data = constraint, aes(x = WS, group = Clhls)) +
-#   geom_line(aes(y = WbW0)) + 
-#   geom_line(aes(y = WbW0Cd0), linetype = 2) +
-#   geom_line(aes(y = WbW0K), linetype = 2) +
-#   ggtitle("Constant Cd0, S values")
+## Battery Fraction ======================================================================
+ggplot(data = constraint, aes(x = WS, group = Clhls)) +
+  geom_line(aes(y = WbW0)) +
+  geom_line(aes(y = WbW0Cd0), linetype = 2) +
+  geom_line(aes(y = WbW0K), linetype = 2) +
+  geom_label(data = filter(constraint, WS == varWS[2]),
+             aes(x = WS, y = WbW0Cd0, label = "Cd0 Contribution"), 
+             hjust = 0, vjust = -0.1, show.legend = FALSE) +
+  geom_label(data = filter(constraint, WS == varWS[2]),
+             aes(x = WS, y = WbW0K, label = "K Contribution"), 
+             hjust = 0, vjust = 1.1, show.legend = FALSE) +
+  ggtitle("Battery Mass Fraction") +
+  geom_label(x = Inf, y = Inf, vjust = 1.5, hjust = 1.1,
+             label = "Constant Cd0 and S")
